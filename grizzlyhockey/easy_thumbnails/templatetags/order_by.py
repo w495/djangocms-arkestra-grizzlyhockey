6a0209d0b0928cc9b07104ad19f8a446dadd01d2
Anton Kukhtichev <toshun@mail.ru>
2014-09-07 01:55:17 +0400

    Sorting players in request and split players at role.

--- grizzlyhockey/easy_thumbnails/templatetags/order_by.py
+++ grizzlyhockey/easy_thumbnails/templatetags/order_by.py
@@ -6,3 +6,14 @@ register = Library()
 def order_by(queryset, args):
     args = [x.strip() for x in args.split(',')]
     return queryset.order_by(*args)
+
+@register.filter_function
+def order_by_game_number(queryset, args):
+    new_query_set = list()
+    for q in queryset:
+        try:
+            new_query_set.append(q)
+        except:
+            pass
+    queryset = sorted(new_query_set, key=lambda tup: int(tup.game_number))
+    return queryset
